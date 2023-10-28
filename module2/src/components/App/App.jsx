ac4edab9ecce650e15d72812941798bf02bcab23
raphael malka <raphael.malka@student.vinci.be>
2023-10-28 14:24:15 +0200

    module2:commited

--- module2/src/components/App/App.jsx
+++ module2/src/components/App/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import Display from '../Display/Display'
 import Button from '../Button/Button'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
+localStorage.setItem("counter", JSON.stringify(0)) 
+
+const App = () => { 
+  const val = JSON.parse(localStorage.getItem("counter"))
+  console.log("valeur du compteur du localstorage: " + val)
+  const [ counter, setCounter ] = useState(parseInt(val))
 
   const changeCount = (delta) => {
-    console.log("change vount")
+    console.log("change count")
     setCounter(counter+delta)
+    localStorage.setItem("counter", JSON.stringify(counter+delta)) 
   }
 
   return (
